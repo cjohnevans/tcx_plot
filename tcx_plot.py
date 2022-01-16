18f0576e5411a645aac27ed9180db6003826ba30
John Evans <c.john.evans@gmail.com>
2022-01-16 13:19:19 +0000

    updated class ActivityData to prepare for multiple files.  Started work on time-axis

--- tcx_plot.py
+++ tcx_plot.py
@@ -16,21 +16,30 @@ sys.path.append('/home/john/code/python-tcxparser/tcxparser')
 import tcxparser
 import matplotlib.pyplot as plt
 
-tcxfile1 = '/home/john/code/tcxpowercomp/activity_8120026129.tcx'
-
-tcx = tcxparser.TCXParser(tcxfile1)
+tcx_garminvector = '/home/john/code/tcx_plot/activity_8120026129.tcx'
+tcx_bikeerg = '/home/john/code/tcx_plot/activity_8120026129.gpx'
 
 class ActivityData:
-    time = tcx.time_values()
-    altitude = tcx.altitude_points()
-    distance = tcx.distance_values()
-    hr = tcx.hr_values()
+    def __init__(self, tcx_file):
+        tcx = tcxparser.TCXParser(tcx_file)
+        self.time = tcx.time_values()
+        self.altitude = tcx.altitude_points()
+        self.distance = tcx.distance_values()
+        self.hr = tcx.hr_values()
+        self.np = len(self.time)
+        self.dt = tcx.time_durations()  # datetime.timedelta object
+        # need to dt to be a sensible type in sensible units (float in sec, min?)
+        print(self.np)
 
-ride_garmin = ActivityData()
+ride_garmin = ActivityData(tcx_garminvector)
 
 fig1 = plt.figure()
 ax1 = fig1.add_subplot(1,1,1)
 ax1.plot(ride_garmin.hr)
 plt.show()
 
-print (ride_garmin.hr[:100])
+print(ride_garmin.dt[:100])
+print(type(ride_garmin.dt[0].seconds))
+print(ride_garmin.dt[20].seconds)
+print(ride_garmin.dt[20].microseconds)
+
